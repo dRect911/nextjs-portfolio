5b61fb7765bada28c9bd2741c93e1b516d75ee10
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-10-24 04:06:02 +0000

    Initialized project a made first section

---
+++ components/Main.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { Email } from '@carbon/icons-react'
+import { FaGithub, FaLinkedinIn, FaDiscord } from 'react-icons/fa'
+
+const Main = () => {
+  return (
+    <>
+    <div className="w-full h-screen text-center flex justify-center items-center" >
+        <div className="group w-2/3 md:w-1/2 p-8 mx-auto my-auto  rounded-2xl border border-neutral-600 shadow-md bg-neutral-50/25 backdrop-blur-sm" >
+            <p className="uppercase text-sm tracking-widest text-neutral-600" >
+                LET'S BUILD SOMETHING TOGETHER
+            </p>
+            <h1 className="py-4 text-neutral-700" >
+                Hi, I'm <span className="bg-clip-text group-hover:text-transparent bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500 transition-all duration-500" >Kenneth</span>
+            </h1>
+            <h1 className="py-2 text-neutral-700" >
+                A Junior Front-End Developer
+            </h1>
+            <p className="py-4 w-[80%] text-neutral-600 m-auto " >
+                I'm a front-end web developer specializing in building exceptional digital experiences. Currently i'm focused on building responsive front-end web applications while learning back-end technologies.
+            </p>
+            <div  className="flex w-1/2 justify-between items-center py-4 m-auto" >
+                <div>
+                    <FaLinkedinIn size={24} className="text-neutral-600" />
+                </div>
+                <div>
+                    <FaGithub size={24} className="text-neutral-600" />
+                </div>
+                <div>
+                    <Email size={24} className="text-neutral-600" />
+                </div>
+                <div>
+                    <FaDiscord size={24} className="text-neutral-600" />
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    {/* dark just testing stuffs */}
+    <div className="w-full h-screen text-center flex justify-center items-center" >
+        <div className="group w-2/3 md:w-1/2 p-8 mx-auto my-auto  rounded-2xl border border-neutral-600 bg-neutral-900/25 shadow-md backdrop-blur-sm" >
+            <p className="uppercase text-sm tracking-widest text-neutral-400" >
+                LET'S BUILD SOMETHING TOGETHER
+            </p>
+            <h1 className="py-4 text-neutral-200" >
+                Hi, I'm <span className="bg-clip-text group-hover:text-transparent bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500 transition-all " >Kenneth</span>
+            </h1>
+            <h1 className="py-4 text-neutral-200" >
+                A Junior Front-End Developer
+            </h1>
+
+        </div>
+    </div>
+    <div className="w-full h-screen text-center flex justify-center items-center" >
+        <div className="group w-2/3 md:w-1/2 p-8 mx-auto my-auto  rounded-2xl border border-neutral-600 shadow-md backdrop-blur-sm" >
+            <p className="uppercase text-sm tracking-widest text-neutral-600" >
+                LET'S BUILD SOMETHING TOGETHER
+            </p>
+            <h1 className="py-4 text-neutral-700" >
+                Hi, I'm <span className="bg-clip-text group-hover:text-transparent bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500 transition-all " >Kenneth</span>
+            </h1>
+            <h1 className="py-4 text-neutral-700" >
+                A Junior Front-End Developer
+            </h1>
+
+        </div>
+    </div>
+    </>
+    
+  )
+}
+
+export default Main
---
+++ components/Navbar.jsx
@@ -0,0 +1,116 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React, {useState} from 'react'
+import { Menu, Close, Email } from '@carbon/icons-react'
+import { FaGithub, FaLinkedinIn, FaDiscord } from 'react-icons/fa'
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
+  return (
+    <div className="fixed w-full z-[100]  h-20  border-b border-neutral-500 backdrop-blur-lg bg-neutral-50/25">
+
+        {/* top nav */}
+        <div className="flex justify-between items-center w-full h-full px-8 " >
+            <Image src="/../public/assets/svg/ccchaos-gradient-squared.png" alt="/" width="50" height="50" className="rounded-full hover:rotate-180 transition-all duration-1000" />
+            {/* <Image src="/../public/assets/svg/sssquiggly.png" alt="/" width="50" height="50"  /> */}
+            <div>
+                <ul className="hidden md:flex items-center" >
+                    <Link href='/' target="_blank">
+                        <li className="ml-10 text-sm uppercase" >Home</li>
+                    </Link>
+                    <Link href='/' target="_blank">
+                        <li className="ml-10 text-sm uppercase" >About</li>
+                    </Link>
+                    <Link href='/' target="_blank">
+                        <li className="ml-10 text-sm uppercase" >Skills</li>
+                    </Link>
+                    <Link href='/' target="_blank">
+                        <li className="ml-10 text-sm uppercase" >Projects</li>
+                    </Link>
+                    <Link href='/' target="_blank">
+                        <li className="ml-10 text-sm uppercase" >Contact</li>
+                    </Link>
+                </ul>
+                <div onClick={handleNav} className='md:hidden cursor-pointer' >
+                    <Menu size={24} />
+                </div>
+            </div>
+        </div>
+
+        {/* side nav */}
+        <div className={ nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/50 transition-all duration-500' : 'transition-all duration-500' } >
+            <div 
+                className={ 
+                    nav 
+                    ? 'md:hidden fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen backdrop-blur-lg bg-neutral-50 p-10 transition-all duration-500' 
+                    : 'fixed left-[-100%] top-0 p-10 transition-all duration-500 '
+                } 
+            >
+
+                <div>
+
+                    <div className="flex w-full items-center justify-between" >
+                        <Image src="/../public/assets/skills/node-js.png" alt="/" width="50" height="50" />
+                        <div onClick={handleNav} className=" cursor-pointer hover:scale-105 hover:rotate-90 transition-all" >
+                            <Close size={24} />
+                        </div>
+                    </div>
+
+                    <div className="border-b my-4" >
+                        <p className="py-4" >Some text here</p>
+                    </div>
+
+                </div>
+
+                <div className="py-4 flex flex-col" >
+                    <ul>
+                        <Link href='/' target="_blank">
+                            <li className="py-4 text-sm hover:translate-x-1 transition-all" >Home</li>
+                        </Link>
+                        <Link href='/' target="_blank">
+                            <li className="py-4 text-sm hover:translate-x-1 transition-all" >About</li>
+                        </Link>
+                        <Link href='/' target="_blank">
+                            <li className="py-4 text-sm hover:translate-x-1 transition-all" >Skills</li>
+                        </Link>
+                        <Link href='/' target="_blank">
+                            <li className="py-4 text-sm hover:translate-x-1 transition-all" >Projects</li>
+                        </Link>
+                        <Link href='/' target="_blank">
+                            <li className="py-4 text-sm hover:translate-x-1 transition-all" >Contact</li>
+                        </Link>
+                    </ul>
+                    <div className="pt-40" >
+                        <p className="uppercase tracking-widest" >
+                            Let's connect
+                        </p>
+                        <div className="flex items-center justify-between my-4 " >
+                            <div>
+                                <FaLinkedinIn size={24} />
+                            </div>
+                            <div>
+                                <FaGithub size={24} />
+                            </div>
+                            <div>
+                                <Email size={24} />
+                            </div>
+                            <div>
+                                <FaDiscord size={24} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
