9687844b0a1e6993ac61aa1c49162a64e7ef26ba
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-10-27 00:47:23 +0000

    fix: disable lints

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 import { Email } from '@carbon/icons-react'
 import { FaGithub, FaLinkedinIn, FaDiscord } from 'react-icons/fa'
