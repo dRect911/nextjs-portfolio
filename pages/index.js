8108dac4f292a122bfb1739123216f54aaeb87c7
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-10-28 12:25:15 +0000

    fix: fixed an import typo

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Main from '../components/Main'
 import Navbar from '../components/Navbar'
 import Projects from '../components/Projects'
 import Skills from '../components/Skills'
-import ccchaos from '../public/ccchaos-gradient-png'
+import ccchaos from '../public/ccchaos-gradient.png'
 
 export default function Home() {
   const [dark, setDark] = useState(false)
