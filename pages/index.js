146e9733a9e99181c23cfd015f49c4d9ec59bcda
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-10-28 14:27:33 +0000

    fix: changed preview image format

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,10 @@ https://i.ibb.co/LztK64V/preview-raw.png */}
         <meta property="og:site_name" content="Kenneth PRINCE AGBODJAN" />
         <meta property="og:title" content="Kenneth | Web Developer" />
         <meta name="description" content="I'm a web developer specializing in building exceptional digital experiences. " />
-        <meta property="og:type" content="website" />
+        <meta property="og:type" content="article" />
         <meta property="og:url" content="http://nextjs-portfolio-drect911.vercel.app/" />
-        <meta property="og:image" content="https://i.ibb.co/LztK64V/preview-raw.png" />
-        <meta property="og:image:secure_url" content="https://i.ibb.co/LztK64V/preview-raw.png" />
+        <meta property="og:image" content="https://i.ibb.co/F6LymfN/preview-light.png" />
+        <meta property="og:image:secure_url" content="https://i.ibb.co/F6LymfN/preview-light.png" />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="400" />
         <meta property="og:image:height" content="225" />
